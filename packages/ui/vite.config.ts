db24eccc9fbac64a100ac478a769db3a12b0cf9f
Jaehyeok <milkboy2564@naver.com>
2023-10-09 22:23:28 +0900

    feat(ui): vite config 파일 설정

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        dts({
+            // .d 파일 생성 디렉토리 지정
+            include: ['lib'],
+        }),
+    ],
+    build: {
+        // public 디렉토리를 dist 폴더에 복사하는 옵션
+        copyPublicDir: false,
+        lib: {
+            entry: resolve(__dirname, 'lib/main.ts'),
+            formats: ['es'],
+            name: '@ui/atoms',
+            fileName: format => `@ui-atoms.${format}.js`,
+        },
+    },
+});
